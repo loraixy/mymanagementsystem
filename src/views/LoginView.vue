92271ebf61858246da2e03b99b0d9f0addde1462
loraixy <2656529351@qq.com>
2023-05-22 09:32:58 +0800

    地点:公司 测试了下图片的响应如何

---
+++ src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div>
+        这里是登录页面
+    </div>
+</template>
--- src/views/NavigatorOne/ItemOne/ItemOne.vue
+++ src/views/NavigatorOne/ItemOne/ItemOne.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('')
+const images = ref('')
 fetch('https://apis.netstart.cn/zhihudaily/story/9741200').then(req => req.json()).then(res => {
-    console.log('数据测试',res)
+    console.log('数据测试', res.story.image)
+    title.value = res.story.title
+    images.value = res.story.image
 })
 </script>
 
 <template>
     <div class="dashboard">
         <h1>This is an dashboard page tIEMone</h1>
+        <h2>{{ title }}</h2>
+        <img :src="images" alt="这是一张图片">
         <ElButton>hello</ElButton>
     </div>
 </template>
