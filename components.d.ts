68cf34b4afcc0cdc8c7baf6e08fb4c61b52bb7c6
loraixy <2656529351@qq.com>
2023-05-22 17:56:02 +0800

    地点: 公司 正在调整样式

--- components.d.ts
+++ components.d.ts
@@ -14,6 +14,7 @@ declare module '@vue/runtime-core' {
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElHeader: typeof import('element-plus/es')['ElHeader']
     ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElInput: typeof import('element-plus/es')['ElInput']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,12 +8,20 @@
             <div class=" flex h-screen relative ">
                 <div class=" flex-1 h-full bg-white "></div>
                 <div class=" flex-1 h-full bg-[#1c3b67] ">这里是登录页面</div>
-                <div
-                    class=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-4/5 h-4/5 drop-shadow-2xl bg-white ">
-                    登录模块
-                    <span class=" drop-shadow ">UserLogin</span>
-                    欢迎登录， Lor 后台管理系统
+                <div class=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-4/5 h-4/5 shadow-lg bg-white ">
+                    <main class=" flex h-full bg-white text-slate-200 ">
+                        <div class=" flex-1 ">
+                            <ElInput placeholder="请输入账号"></ElInput>
+                            <ElInput placeholder="请输入密码"></ElInput>
+                            <ElInput placeholder="请输入内容"></ElInput>
 
+                        </div>
+                        <div class=" flex-1 h-full bg-[#1c3b67]">
+                            登录模块
+                            <span class=" drop-shadow ">UserLogin</span>
+                            欢迎登录， Lor 后台管理系统
+                        </div>
+                    </main>
                 </div>
             </div>
         </div>
