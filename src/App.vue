a0016077c756e7619ddfca93713ac50d3c58dbac
loraixy <2656529351@qq.com>
2023-05-23 17:48:02 +0800

    '在app.vue添加了个watch'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
+import { watch } from 'vue'
 // import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
+
+const route = useRoute()
+console.log(route)
+
+watch(() => route.fullPath, (val) => {
+  console.log(val)
+})
 
 
 </script>
