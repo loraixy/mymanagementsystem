9336c2a06df580ef081059c065ac91aa889739f8
loraixy <2656529351@qq.com>
2023-05-27 16:56:13 +0800

    更新gitpages

--- src/views/pages/Navigatorhhhhh/HhhhView.vue
+++ src/views/pages/Navigatorhhhhh/HhhhView.vue
@@ -6,13 +6,17 @@ const hospitalList = ref([])
 fetch('../../../../医院报表.json').then(req => req.json()).then(res => {
     hospitalList.value = res.data[0]
     console.log("hospitalList =>", hospitalList.value)
+
+
 })
 </script>
 
 <template>
-    <div>我哈哈哈哈哈哈
+    <div>
+        这是一个表格
         <ElTable :data="hospitalList">
             <ElTableColumn prop="XINGM" label="姓名" ></ElTableColumn>
         </ElTable>
     </div>
 </template>
+
