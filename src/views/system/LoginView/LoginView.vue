6e685765fdd36c09efe8a163a22f2fc10ea541d9
loraixy <2656529351@qq.com>
2023-05-28 18:54:29 +0800

    更新一下登陆页面的动画

--- src/views/system/LoginView/LoginView.vue
+++ src/views/system/LoginView/LoginView.vue
@@ -16,25 +16,25 @@ import LoginModel from './components/LoginModel.vue'
         <div>
             <div class=" flex h-screen relative ">
                 <div class=" flex-1 h-full bg-white "></div>
-                <div class=" flex-1 h-full bg-[--lor-blue] ">这里是登录页面</div>
+                <div class=" flex-1 h-full bg-[--lor-blue] "></div>
                 <Transition name="card">
                     <div class=" login-card " v-show="showLoginModel">
                         <main class=" flex h-full  rounded-sm bg-white text-slate-200 ">
-                            <div class=" flex-1 flex flex-col justify-center items-center bg-gray-100 ">
-                                <el-row class="mb-4">
+                            <div class=" flex flex-col justify-center items-center w-1/2 bg-gray-100 max-md:w-full transition-all  ">
+                                <!-- <el-row class="mb-4">
                                     <el-button>Default</el-button>
                                     <el-button type="primary">Primary</el-button>
                                     <el-button type="success">Success</el-button>
                                     <el-button type="info">Info</el-button>
                                     <el-button type="warning">Warning</el-button>
                                     <el-button type="danger">Danger</el-button>
-                                </el-row>
+                                </el-row> -->
                                 <div class=" w-[18.75rem] p-7 rounded-sm text-gray-400 bg-white ">
                                     <!-- 登录表单组件 -->
                                     <LoginModel></LoginModel>
                                 </div>
                             </div>
-                            <div class=" flex-1 h-full bg-[--lor-blue]">
+                            <div class=" h-full w-1/2 bg-[--lor-blue] max-md:hidden ">
                                 登录模块
                                 <span class=" drop-shadow ">UserLogin</span>
                                 欢迎登录， Lor 后台管理系统
