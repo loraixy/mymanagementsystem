92bd35d34bef3e54df7e5065cf75d351092b7a1c
loraixy <2656529351@qq.com>
2023-05-31 23:07:36 +0800

    写了点例子

--- src/views/pages/NavigatorOnetwo/GroupOne.vue
+++ src/views/pages/NavigatorOnetwo/GroupOne.vue
@@ -1,6 +1,19 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+// 树形数据
 const treeData = ref<any[]>([])
+
+const num1 = ref(0)
+
+const num2 = ref(1)
+
+const resultNum = computed(() => {
+    return (num1.value * 100 - num2.value * 100) / 100
+})
+
+const multiplicationNun = computed(() => {
+    return num1.value * num2.value
+})
 
 fetch('treeData.json').then(req => req.json()).then(res => {
     treeData.value = res.data[0]
@@ -40,8 +53,11 @@ function addGourpNumToSqdMxList(treeData: any, gourpNumField: any) {
 <template>
     <div>树形数据修改
         <div>
+            <ElInput type="text" v-model="num1" />
+            <ElInput type="text" v-model="num2" />
+            <p>使用先将小数转换整数后再转换回来(只能解决固定返回的小数){{ resultNum }}</p>
+            <p>{{ multiplicationNun }}</p>
             <p>这里是js经典的精度问题</p>
-
         </div>
     </div>
 </template>
