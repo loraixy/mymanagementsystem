db1a497b75118d0a4d38ba05e09175a3ebffce1c
loraixy <2656529351@qq.com>
2023-06-01 10:45:58 +0800

    更改了运算规则

--- src/views/pages/NavigatorOnetwo/GroupOne.vue
+++ src/views/pages/NavigatorOnetwo/GroupOne.vue
@@ -2,18 +2,38 @@
 import { ref, computed } from 'vue'
 // 树形数据
 const treeData = ref<any[]>([])
-
+// 数1
 const num1 = ref(0)
-
+// 数2
 const num2 = ref(1)
+// 减法的最终运算结果
+const resultNum = computed<number>((): number => {
+    const decimalPlaces1 = countDecimalPlaces(num1.value)
 
-const resultNum = computed(() => {
-    return (num1.value * 100 - num2.value * 100) / 100
+    const decimalPlaces2 = countDecimalPlaces(num2.value)
+
+    let resultPlaces: number = 0
+
+    decimalPlaces1 > decimalPlaces2 ? resultPlaces = decimalPlaces1 : resultPlaces = decimalPlaces2
+
+    return (num1.value * (10 ** resultPlaces) - num2.value * (10 ** resultPlaces)) / (10 ** resultPlaces)
 })
 
 const multiplicationNun = computed(() => {
     return num1.value * num2.value
 })
+
+// 判断几位小数
+const countDecimalPlaces = (num: number | string): number => {
+    // 判断是否有.没有的话就直接返回0， 就是没有小数
+    let decimaIndex: number = num.toString().indexOf('.')
+
+    if (decimaIndex === -1) {
+        return 0
+    }
+    // 这边直接在.的位置到末尾的字符串，然后返回该字符串长度就能得到该小数有几位了
+    return num.toString().substring(decimaIndex + 1, num.toString().length).length
+}
 
 fetch('treeData.json').then(req => req.json()).then(res => {
     treeData.value = res.data[0]
@@ -58,7 +78,7 @@ function addGourpNumToSqdMxList(treeData: any, gourpNumField: any) {
             <ElInput type="text" v-model="num2" />
             <p>使用先将小数转换整数后再转换回来(只能解决固定返回的小数){{ resultNum }}</p>
             <p>{{ multiplicationNun }}</p>
-            
+
         </div>
     </div>
 </template>
