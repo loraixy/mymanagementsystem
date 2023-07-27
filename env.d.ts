4db42efac051fbcd29df32e3c7970f1d7051ad60
loraixy <2656529351@qq.com>
2023-07-28 00:09:55 +0800

    添加数据

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,14 @@
 /// <reference types="vite/client" />
 
+// 为了告诉 TypeScript 这些新 property，我们需要使用[模块扩充]
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        $parentHost: string
+        $parentPort: string
+        $title: string
+    }
+}
+
 interface ImportMetaEnv {
     readonly VITE_APP_TITLE: string
     readonly VITE_NODE_ENV: string
